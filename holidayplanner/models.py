7d5b03a8d270a5700fb6025b979d0812110da81d
hitesh.savaliya@gmail.com <hitesh.savaliya@gmail.com>
2012-12-27 00:31:24 +0000

    o Employee.objects.all

--- holidayplanner/models.py
+++ holidayplanner/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Employee (models.Model):
+	def __unicode__(self):
+		return self.name;
 	name = models.CharField(max_length=256)
 	empId = models.CharField(max_length=256)
 	startDate = models.DateTimeField('start Date')
